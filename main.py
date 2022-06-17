e387388166ccf8b51b7d79bd86dac26ddcec1e40
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-17 16:09:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+Input = input("Please input. ")
+plusOne = Input + 1
+print(plusOne)
+#when you type an input statement without any converters, such as int or float, what you input will be a string. Because it is a string, you cannot add any predetermined numbers to it for numbers are automatically integers, not string.
